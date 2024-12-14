ae4ed96eabb78f66252e89151e0d511cfd137697
Alok garg <87581471+alokgarg003@users.noreply.github.com>
2024-12-15 00:55:03 +0530

    Update AuthController.java

--- src/main/java/com/cdac/controller/AuthController.java
+++ src/main/java/com/cdac/controller/AuthController.java
@@ -1,52 +1,4 @@
-//package com.cdac.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import com.cdac.config.JwtUtil;
-//import com.cdac.model.JwtRequest;
-//import com.cdac.model.JwtResponse;
-//
-//@RestController
-//@CrossOrigin(origins = "http://localhost:3000")
-//public class AuthController {
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private JwtUtil jwtTokenUtil;
-//
-//    @Autowired
-//    private UserDetailsService userDetailsService;
-//
-//    @PostMapping("/authenticate")
-//    public JwtResponse createAuthenticationToken(@RequestBody JwtRequest authenticationRequest) throws Exception {
-//
-//        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-//
-//        final UserDetails userDetails = userDetailsService.loadUserByUsername(authenticationRequest.getUsername());
-//
-//        final String token = jwtTokenUtil.generateToken(userDetails.getUsername());
-//
-//        return new JwtResponse(token);
-//    }
-//
-//    private void authenticate(String username, String password) throws Exception {
-//        try {
-//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-//        } catch (Exception e) {
-//            throw new Exception("INVALID_CREDENTIALS", e);
-//        }
-//    }
-//}
+
 package com.cdac.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -70,7 +22,7 @@ import com.cdac.model.JwtResponse;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+@CrossOrigin(origins = "https://web-xi-orcin.vercel.app", allowCredentials = "true")
 @Slf4j
 public class AuthController {
 
