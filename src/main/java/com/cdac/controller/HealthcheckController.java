94f5e8431f62f06e9868fdd5785dd2cf60b64484
Alok garg <87581471+alokgarg003@users.noreply.github.com>
2024-12-15 00:55:42 +0530

    Update HealthcheckController.java

--- src/main/java/com/cdac/controller/HealthcheckController.java
+++ src/main/java/com/cdac/controller/HealthcheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://web-xi-orcin.vercel.app")
 public class HealthcheckController {
 	
 	@GetMapping("/healthcheck")
